8c3a3558d697b3cafe7f584c4c7892c275a94d6a
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-03-16 21:30:14 -0400

    Added struct and function declarations to istream.

---
+++ lib/istream.h
@@ -0,0 +1,16 @@
+#ifndef __cplusplus
+#ifndef Included_istream_h
+#define Included_istream_h
+#include<stdio.h>
+#include<stdlib.h>
+struct __istream;
+int closeis(struct __istream*);
+struct __istream*openis(FILE*f);
+struct __istream*openifs(const char*);
+char cpcio_getc_is(struct __istream*);
+char*cpcio_gtoken_is(struct __istream*);
+void cpcio_use_delim(struct __istream*,const char*);
+char*cpcio_get_delim(struct __istream*);
+typedef struct __istream*istream;
+#endif
+#endif
