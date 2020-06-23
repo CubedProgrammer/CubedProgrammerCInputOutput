e52687ba85b9f6d34c053c50c1d96362111cc838
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-06-23 16:41:23 -0400

    Made zero-arg functions force zero-arg.

--- lib/sstream.h
+++ lib/sstream.h
@@ -6,7 +6,7 @@
 struct __isssrc;
 struct __ossdest;
 struct __istream*openiss(const char*);
-struct __ostream*openoss();
+struct __ostream*openoss(void);
 char*cpcio_oss_str(struct __ostream*);
 int __cpcio_read_iss(void*,char*,size_t);
 int __cpcio_write_oss(void*,const char*,size_t);
